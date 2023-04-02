ecd161440ee661a256c8b08859bef901b6725f15
Michel KACOU <Your email>
2023-04-02 22:24:12 +0200

    fix fetch issue

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,14 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
-  const genreTitle = "Rap";
-  console.log(genres);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = "Pop";
+  console.log(data, error);
 
+  if (isFetching) return <Loader title="Loading songs ... " />;
+  if (error) return <Error />;
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -24,7 +28,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gp-10">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -10,7 +10,7 @@ const options = {
 
 fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
   .then((response) => response.json())
-  .then((response) => console.log(response))
+  .then((response) => console.log("1111111111111111111111111", response))
   .catch((err) => console.error(err));
 
 export const shazamCoreApi = createApi({
@@ -18,15 +18,18 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     prepareHeaders: (headers) => {
+      console.log(headers);
       headers.set(
-        "​X-RapidAPI-Key",
+        "X-RapidAPI-Key",
         "9f18215112mshb5f8b662a048d78p1ee5ddjsn451a6dc1856d"
       );
+      headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com");
+      console.log(headers);
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: "/charts/world" }),
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
   }),
 });
 
