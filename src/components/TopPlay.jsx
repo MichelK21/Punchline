28261687503b4f3130ee450ba0104da2944dbc3c
Michel KACOU <Your email>
2023-04-06 20:53:42 +0200

    top play functionnality added

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -4,13 +4,45 @@ import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode } from "swiper";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 import "swiper/css";
 import "swiper/css/free-mode";
+import PlayPause from "./PlayPause";
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c42] py-2 p-4 rounded-lg cursor-pointer">
-    {song.title}
+    <h3 className="fot-bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      {/* {console.log(song.images?.background)} */}
+      <img
+        className="w-20 h-20 rounded-lg"
+        src={song.images?.coverarthq}
+        alt={song?.title}
+      />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick}
+    />
   </div>
 );
 
@@ -29,7 +61,7 @@ const TopPlay = () => {
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -48,7 +80,15 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col gap-1 ">
           {topPlays?.map((song, i) => (
-            <TopChartCard key={song.key} song={song} i={i} />
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
           ))}
         </div>
       </div>
@@ -59,6 +99,31 @@ const TopPlay = () => {
           <p className="text-gray-300 text-base cursor-pointer"> See more</p>
         </Link>
       </div>
+
+      <Swiper
+        slidesPerView="auto"
+        spaceBetween={15}
+        freeMode
+        centeredSlidesBounds
+        modules={[FreeMode]}
+        className="mt-4"
+      >
+        {topPlays?.map((song, i) => (
+          <SwiperSlide
+            key={song?.key}
+            style={{ width: "25%", height: "auto" }}
+            className="shadow-lg rounded-full animate-slideright"
+          >
+            <Link to={`/artists/${song?.artists[0].adamid}`}>
+              <img
+                src={song?.images.background}
+                alt="name"
+                className="rounded-full w-full object-cover"
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
